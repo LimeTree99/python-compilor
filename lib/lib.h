a07ec0f5efb8eb45d151d9eaf3f65e042b75a887
erinhemsley <102770017+erinhemsley@users.noreply.github.com>
2023-11-04 11:48:49 -0400

    established a better file order

---
+++ lib/lib.h
@@ -0,0 +1,14 @@
+#include "regex.h"
+
+/*
+This file is used to make importing the /lib library easier.
+It creates a clear entry point to the library. All layout of
+/lib will be described here.
+
+import order:
+    lib
+    regex
+    log
+    string
+    imports
+*/
--- lib/main.c
+++ lib/main.c
@@ -1,4 +1,4 @@
-#include "regex.h"
+#include "lib.h"
 
 /*
 order:
